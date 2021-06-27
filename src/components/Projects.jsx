7166351e1280d57ddfaa436dd50610ed3f20a243
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-27 03:23:19 -0600

    Changed projects to display in carousel instead of card deck

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Card from 'react-bootstrap/Card';
+// import CardDeck from 'react-bootstrap/CardDeck';
+// import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Projects(Content) {
    const portfolio = Content.Content.portfolio;
@@ -9,10 +10,29 @@ function Projects(Content) {
    return (
       <Container className="bg-dark">
          <h2 className="justify-content-center">Key Projects I have done or contributed to</h2>
-         <CardDeck bg="dark">
+         <Carousel bg="secondary" className="mb-6" fade>
             {portfolio.map((item) => {
                return (
-                  <Card bg="secondary" text="light" style={{ width: '20rem' }} className="mb-3">
+                  <Carousel.Item key={item.key}>
+                     <img 
+                        className="d-block w-50 mx-auto"
+                        src={item.image}
+                        alt={item.name}
+                     />
+                     <Carousel.Caption className="text-light">
+                        <h3>{item.name}</h3>
+                        <p>{item.description}</p>
+                        <p><a href={item.link}>Check it out!</a></p>
+                        <p><a href={item.repo}>View code</a></p>
+                     </Carousel.Caption>
+                  </Carousel.Item>
+               )
+            })}
+         </Carousel>
+         {/* <CardDeck bg="secondary" className="mb-6">
+            {portfolio.map((item) => {
+               return (
+                  <Card bg="secondary" text="light" style={{ width: '35rem' }} className="mb-3" key={item.key}>
                      <Card.Img variant="top" src={item.image} />
                      <Card.Body>
                         <Card.Title>{item.name}</Card.Title>
@@ -25,7 +45,7 @@ function Projects(Content) {
                   </Card>
                )
             })}
-         </CardDeck>
+         </CardDeck> */}
       </Container>
    )
 };
