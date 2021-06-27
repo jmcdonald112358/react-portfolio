a8c971126598b581dec5ddc62917536de2fcc93b
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-26 23:43:34 -0600

    Added outline of portfolio content file with one project entry for testing

---
+++ src/assets/Content.jsx
@@ -0,0 +1,48 @@
+let Content = {
+   "portfolio": [
+      {
+         "name": "Garden Overflow",
+         "description": "A gardening community",
+         "image": "../assets/GardenOverflow.jpg",
+         "link": "https://garden-overflow.herokuapp.com/",
+         "repo": "https://github.com/arianaw15/Garden-Overflow"
+      },
+      {
+         "name": "",
+         "description": "",
+         "image": "",
+         "link": "",
+         "repo": ""
+      },
+      {
+         "name": "",
+         "description": "",
+         "image": "",
+         "link": "",
+         "repo": ""
+      },
+      {
+         "name": "",
+         "description": "",
+         "image": "",
+         "link": "",
+         "repo": ""
+      },
+      {
+         "name": "",
+         "description": "",
+         "image": "",
+         "link": "",
+         "repo": ""
+      },
+      {
+         "name": "",
+         "description": "",
+         "image": "",
+         "link": "",
+         "repo": ""
+      }
+   ]
+};
+
+export default Content;
