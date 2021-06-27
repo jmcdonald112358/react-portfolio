c0ffc23d1541b86a7d8cc31166fc2c285847c9ea
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-26 23:40:46 -0600

    Added content for header and routing

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function Header() {
+   return (
+      <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+         <LinkContainer to="https://github.com/jmcdonald112358">
+            <Navbar.Brand>
+               Justin McDonald
+            </Navbar.Brand>
+         </LinkContainer>
+         <LinkContainer to="/About">
+            <Nav.Link>About</Nav.Link>
+         </LinkContainer>
+         <LinkContainer to="/Portfolio">
+            <Nav.Link>Portfolio</Nav.Link>
+         </LinkContainer>
+         <LinkContainer to="/Contact">
+            <Nav.Link>Contact</Nav.Link>
+         </LinkContainer>
+      </Navbar>
+   )
+}
+
+export default Header;
