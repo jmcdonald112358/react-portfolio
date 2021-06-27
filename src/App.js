b1849911342de0553d0a825125025b893d76ae24
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-26 23:38:34 -0600

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Invalid from './pages/Invalid';
+import Portfolio from './pages/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-dark">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route component={Invalid} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
