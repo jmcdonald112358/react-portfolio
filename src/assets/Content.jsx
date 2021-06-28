3d74ffcee0d6643d68db8e514e6ab9b158afd538
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-27 19:16:40 -0600

    Added details for other projects

--- src/assets/Content.jsx
+++ src/assets/Content.jsx
@@ -2,50 +2,50 @@ let Content = {
    "portfolio": [
       {
          "name": "Garden Overflow",
-         "description": "A gardening community",
+         "description": "A gardening community web app for users to share tips and tricks related to growing plants that are viable in their respective hardiness zone.",
          "image": "images/GardenOverflow.jpg",
          "link": "https://garden-overflow.herokuapp.com/",
          "repo": "https://github.com/arianaw15/Garden-Overflow",
          "key": "1"
       },
       {
-         "name": "",
-         "description": "",
-         "image": "",
-         "link": "",
-         "repo": "",
+         "name": "Barkle",
+         "description": "A web app for dog owners to connect with other local dog owners to coordinate dog-friendly meetups and playdates.",
+         "image": "images/Barkle.jpg",
+         "link": "https://barkle.herokuapp.com/",
+         "repo": "https://github.com/Jpeyton-hub/Barkle",
          "key": "2"
       },
       {
-         "name": "",
-         "description": "",
-         "image": "",
-         "link": "",
-         "repo": "",
+         "name": "Art Tracker",
+         "description": "A web app for searching an artist or ethnography to find galleries and museums where the artwork can be viewed in-person.",
+         "image": "images/Art_Tracker.jpg",
+         "link": "https://jmcdonald112358.github.io/Art-tracking-app/",
+         "repo": "https://github.com/jmcdonald112358/Art-tracking-app",
          "key": "3"
       },
       {
-         "name": "",
-         "description": "",
+         "name": "Employee Directory",
+         "description": "A simple web app for viewing a directory of employees in an organization, including a picture of each employee and their relevant details.",
          "image": "",
          "link": "",
-         "repo": "",
+         "repo": "https://github.com/jmcdonald112358/Employee-Directory",
          "key": "4"
       },
       {
-         "name": "",
-         "description": "",
+         "name": "Employee Management",
+         "description": "A simple node.js app for viewing, adding, modifying, and deleting employee information relevant to their position in the company.",
          "image": "",
          "link": "",
-         "repo": "",
+         "repo": "https://github.com/jmcdonald112358/employee-management",
          "key": "5"
       },
       {
-         "name": "",
-         "description": "",
-         "image": "",
-         "link": "",
-         "repo": "",
+         "name": "Note Taker Express",
+         "description": "A simple web app for taking notes that can be accessed or deleted later on.",
+         "image": "images/Note-Taker.jpg",
+         "link": "https://secret-temple-27869.herokuapp.com/notes",
+         "repo": "https://github.com/jmcdonald112358/note-taker-express",
          "key": "6"
       }
    ]
