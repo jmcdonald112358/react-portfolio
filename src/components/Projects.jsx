9c308df5c2bc6c33b3b3a59843c1d32ce94b5517
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-27 19:15:13 -0600

    Changed back to a card-based view and other styling tweaks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,38 +1,18 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-// import CardDeck from 'react-bootstrap/CardDeck';
-// import Card from 'react-bootstrap/Card';
-import Carousel from 'react-bootstrap/Carousel';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
 
 function Projects(Content) {
    const portfolio = Content.Content.portfolio;
 
    return (
-      <Container className="bg-dark">
-         <h2 className="justify-content-center">Key Projects I have done or contributed to</h2>
-         <Carousel bg="secondary" className="mb-6" fade>
+      <Container>
+         <h2 className="text-center text-success my-3">Key Projects I have completed or contributed to</h2>
+         <CardDeck style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginBottom: '35px' }}>
             {portfolio.map((item) => {
                return (
-                  <Carousel.Item key={item.key}>
-                     <img 
-                        className="d-block w-50 mx-auto"
-                        src={item.image}
-                        alt={item.name}
-                     />
-                     <Carousel.Caption className="text-light">
-                        <h3>{item.name}</h3>
-                        <p>{item.description}</p>
-                        <p><a href={item.link}>Check it out!</a></p>
-                        <p><a href={item.repo}>View code</a></p>
-                     </Carousel.Caption>
-                  </Carousel.Item>
-               )
-            })}
-         </Carousel>
-         {/* <CardDeck bg="secondary" className="mb-6">
-            {portfolio.map((item) => {
-               return (
-                  <Card bg="secondary" text="light" style={{ width: '35rem' }} className="mb-3" key={item.key}>
+                  <Card bg="secondary" text="light" style={{ width: '400px'}} className="mx-3 mb-3" key={item.key}>
                      <Card.Img variant="top" src={item.image} />
                      <Card.Body>
                         <Card.Title>{item.name}</Card.Title>
@@ -45,7 +25,7 @@ function Projects(Content) {
                   </Card>
                )
             })}
-         </CardDeck> */}
+         </CardDeck>
       </Container>
    )
 };
